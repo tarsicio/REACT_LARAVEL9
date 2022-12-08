ce36cf389e609cbc618a4448bcea8afa213f653b
Tarsicio Carrizales <telecom.com.ve@gmail.com>
2022-12-08 15:40:05 -0400

    Modificando NavBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Navmenu from './Components/Navmenu/Navmenu';
+//import Navmenu from './Components/Navmenu/Navmenu';
 import Navigationbar from './Components/Navigationbar/Navigationbar';
-import Footer from './Components/Footer/Footer'
+//import Footer from './Components/Footer/Footer'
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -9,8 +9,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <Navigationbar />
-      <Footer />
+      <Navigationbar />      
     </>
   ); 
 }
--- src/Components/Navigationbar/Navigationbar.js
+++ src/Components/Navigationbar/Navigationbar.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import LogoHorus from '../../assets/img/horus.png';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from '../Home/Home';
@@ -12,41 +14,57 @@ import Dashboard from '../../Pages/Dashboard/Dashboard';
 function Navigationbar() {
   return (
     <>
-    <Router>
-    <Navbar>
-      <Container>
-        <Link className="navbar-brand" to={'/'}>
-          <img src={LogoHorus} style={{width: 30, height: 30,}} alt="Logo_Post"  className="img-fluid" />              
-        </Link>
-        <Navbar.Brand>HORUS</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to={'/login'}>
-                Login
+    <Router> 
+      {['xxl'].map((expand) => (
+        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+          <Container fluid>
+            <Link className="navbar-brand" to={'/'}>
+              <img src={LogoHorus} style={{width: 30, height: 30,}} alt="Logo_Post"  className="img-fluid" />              
               </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to={'/register'}>
-                Register
-              </Link>
-            </li>
-          </ul>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <div className="auth-wrapper">
-      <div className="auth-inner">
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
-    </div>
-    </Router>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  <Link className="navbar-brand" to={'/'}>
+                    <img src={LogoHorus} style={{width: 30, height: 30,}} alt="Logo_Post"  className="img-fluid" />              
+                </Link>
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/login'}>
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/register'}>
+                      Register
+                    </Link>
+                  </li>
+                </ul>
+                </Nav>                
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+          </div>
+        </div>
+      </Router> 
     </>
   );
 }
