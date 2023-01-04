666e5279b4074d22b55f8e95019fc0d27011de33
Tarsicio Carrizales <telecom.com.ve@gmail.com>
2023-01-03 21:51:09 -0400

    Modificando el reloj

--- src/Components/utils/reloj/Reloj.js
+++ src/Components/utils/reloj/Reloj.js
@@ -1,24 +1,29 @@
 import { useEffect, useRef } from "react";
 
 function Reloj() {
-  const h1 = useRef();
-  const ti = () => {
-    const fechahora = new Date();
-    const hora = fechahora.getHours();
-    const minuto = fechahora.getMinutes();
-    const segundo = fechahora.getSeconds();
+  //const h1Ref = useRef();
+  //<h2 ref={h1Ref}>{horario()}</h2>, colocar esta etiqueta en el lugas mas apropiado     
+  const h2Ref = useRef();
+  /*const horario = () => {
+    const tiempo = new Date();
+    const hora = tiempo.getHours();
+    const minuto = tiempo.getMinutes();
+    const segundo = tiempo.getSeconds();
     return `${hora}:${minuto}:${segundo}`;
+  };*/
+  const fecha = () => {
+    return Date();
   };
   useEffect(() => {
     const cl = setInterval(() => {
-      h1.current.innerHTML = `Time | Hora: ${ti()}`;
+      //h1Ref.current.innerHTML = `Time | Hora: ${horario()}`;
+      h2Ref.current.innerHTML = `${fecha()}`;
     }, 1000);
     return () => clearInterval(cl);
-  }, []);
+  }, []);  
   return (
-    <div className="App" style={{padding:30}}>
-      <h1 ref={h1}>{ti()}</h1>
-      <h2>Venezuela</h2>     
+    <div className="App" style={{padding:30}}>      
+      <h2 ref={h2Ref}>{fecha()}</h2>     
     </div>
   );
 }
