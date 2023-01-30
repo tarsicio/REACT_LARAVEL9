2aa9e6ec450297e367a32c7f696f731be9e1570d
Tarsicio Carrizales <telecom.com.ve@gmail.com>
2023-01-30 01:24:53 -0400

    Se modifica archivo de rutas e imagenes

--- src/config/rutas/rutas.tsx
+++ src/config/rutas/rutas.tsx
@@ -6,16 +6,29 @@
  * @license MIT
  * @copyright (c) 2023 Tarsicio Carrizales
  */
-
+// URL BASE ENDPOIND
 export const URL_BASE         = 'http://localhost:8000/api/v1'; 
+
+// URL RAIZ
 export const HOME             = '/';
+
+// URL LOGIN LOGOUT
 export const LOGIN            = '/login';
 export const LOGOUT           = '/logout';
+
+// URL REGISTER
 export const REGISTER         = '/register';
 export const CONFIRM          = '/register/confirm/:confirmation_code';
-export const DASHBOARD        = '/dashboard';
+
+// URL RECOVERY Y RESET PASSWORD
 export const REQUEST_PASSWORD = '/recovery/password';
 export const SEND_MAIL        = '/reset';
 export const RESET_PASSWORD   = '/password/reset/:reset_password';
 export const PASSWORD_RESET   = '/password/reset/';
+
+// URL BIENVENIDA
 export const BIENVENIDO       = '/bienvenido';
+
+// URL DASHBOARD
+export const DASHBOARD        = '/dashboard';
+
