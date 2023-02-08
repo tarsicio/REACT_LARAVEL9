1aa55fd73e16a8506cc9f96609b0cc094c0f87c1
Tarsicio Carrizales <telecom.com.ve@gmail.com>
2023-02-08 13:53:34 -0400

    Se procede a construir el Dashboard principal de la aplicación

--- src/Components/Header/Sidebar.tsx
+++ src/Components/Header/Sidebar.tsx
@@ -1,34 +1,65 @@
-import React from 'react';
+import React, { useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import './sidebar.css';
+import { NavLink } from 'react-router-dom';
+import { HOME, LOGIN } from '../../config/rutas/rutas';
+import { useTranslation } from 'react-i18next';
+import '../Footer/background.css';
 
-function Sidebar() {
+function SideBar(props) {
+  const  { t, i18n } = useTranslation();
+  const sidebarClass = props.isOpen ? "sidebar open" : "sidebar";
+
   return (
-    <div className="sidebar" style={{padding:"10px", backgroundColor: "#EAECEE"}}>
+    <div className={sidebarClass} >
+      <button onClick={props.toggleSidebar} className="sidebar-toggle">
+        X
+      </button>
       <ul style={{listStyleType: 'none'}}>
         <li>
-          <a href="#" className="sidebar-link" style={{textDecoration: 'none'}}>Link 1</a>
+          <NavLink className="nav-link" to={HOME} style={{color: "white", padding:"10px"}}>
+            {t('sidebar.dashboard')}
+          </NavLink>
         </li>
         <li>
-          <a href="#" className="sidebar-link" style={{textDecoration: 'none'}}>Link 2</a>
+          <NavLink className="nav-link" to={HOME} style={{color: "white", padding:"10px"}}>
+            {t('sidebar.users')}
+          </NavLink>
         </li>
         <li>
-          <a href="#" className="sidebar-link" style={{textDecoration: 'none'}}>Link 3</a>
+          <NavLink className="nav-link" to={HOME} style={{color: "white", padding:"10px"}}>
+            {t('sidebar.notifications')}
+          </NavLink>
         </li>
       </ul>
       <div className="sidebar-dropdown">
         <ul style={{listStyleType: 'none'}}>
           <li>
-            <a href="#" className="dropdown-link" style={{textDecoration: 'none'}}>Dropdown 1</a>
+            <NavLink className="nav-link" to={HOME} style={{color: "white", padding:"10px"}}>
+            {t('sidebar.rols')}
+          </NavLink>
           </li>
           <li>
-            <a href="#" className="dropdown-link" style={{textDecoration: 'none'}}>Dropdown 2</a>
+            <NavLink className="nav-link" to={HOME} style={{color: "white", padding:"10px"}}>
+            {t('sidebar.module')}
+          </NavLink>
           </li>
           <li>
-            <a href="#" className="dropdown-link" style={{textDecoration: 'none'}}>Dropdown 3</a>
+            <NavLink className="nav-link" to={HOME} style={{color: "white", padding:"10px"}}>
+            {t('sidebar.permission')}
+          </NavLink>
           </li>
         </ul>
       </div>
+      <ul style={{listStyleType: 'none'}}>
+        <li>
+          <NavLink className="nav-link" to={HOME} style={{color: "white", padding:"10px"}}>
+            {t('sidebar.color')}
+          </NavLink>
+        </li>
+      </ul>
     </div>
   );
 }
 
-export default Sidebar;
+export default SideBar;
--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -7,29 +7,33 @@
  * @copyright (c) 2023 Tarsicio Carrizales
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'react-bootstrap/Image'
 import { Container, Row, Col } from 'react-bootstrap';
 import { LOGO_HORUS, URL_STORAGE } from '../../config/imgs/imgs';
 import { UseData } from '../../store/UserLogin';
-import Sidebar from '../../components/Header/Sidebar';
+import SideBar from '../../components/Header/SideBar';
 import { useTranslation } from 'react-i18next';
 
 function Dashboard(){	
 	const userData = UseData(state => state._user);
 	const  { t, i18n } = useTranslation();
+	const [sidebarOpen, setSideBarOpen] = useState(false);
+  	const handleViewSidebar = () => {
+    	setSideBarOpen(!sidebarOpen);
+  	};	
 	return(
-		<div style={{padding:"5px", backgroundColor:"white"}}>
+		<div style={{padding:"1px", backgroundColor:"white"}}>
 		    <Row>
 		        <Col md='2'>
-		            <Sidebar />
+		            <SideBar isOpen={sidebarOpen} toggleSidebar={handleViewSidebar} />
 		        </Col>
 		        <Col md='10'>
 		            <section>			
 						<div className="">
 							<div className="">								
 								<h1 style={{textAlign:"center"}}>{t('dashboard.title')}</h1>								
-					       	</div>						
+					       	</div>					       	
 						</div>			
 					</section>
 		        </Col>
